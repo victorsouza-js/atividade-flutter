771f2e4c779408c7ebdb14ce893e1c2d022c7f6e
victorsouza-js <victorsouza5611@gmail.com>
2025-03-17 19:35:39 -0300

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,159 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: Column(
+          
           children: [
-            SizedBox(height: 400,),
-          Center(
-            child: Text('Get your Money',style: TextStyle(fontSize: 30, color: Colors.white),),  
-        ),
-        Container(
-          child: Text('Under Control',style: TextStyle(color: Colors.white,fontSize: 20),),
-        ),
-        Padding(padding: EdgeInsets.only(top: 20)),
-        Container(
-          child: Text('Manage your expenses',style: TextStyle(color: Colors.white),),
-        ), 
-        Container(
-          child: Text('Seamlessly',style: TextStyle(color: Colors.white),),
-        ),
-        SizedBox(height: 300,),
-        
-        Container(
-          child: Text( 'Sing Up with E-mail ID',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),  textAlign: TextAlign.center,),
-          width: 220, height: 35, color: Colors.deepPurple,
-        ),
-        ]
+           Padding(padding: EdgeInsets.only(top: 200)),
+            Center(
+              child: SizedBox(
+                width: 100,
+                height: 100,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 5,
+                      left: 0,
+                      child: Container(
+                        width: 45,
+                        height: 45,
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF6B5ECD),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: Container(
+                        width: 50,
+                        height: 100,
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF6B5ECD),
+                          borderRadius: BorderRadius.only(
+                             bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(0),
+                            topLeft: Radius.circular(0),
+                            topRight: Radius.circular(50),
+                          
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      child: Container(
+                        width: 45,
+                        height: 45,
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF6B5ECD),
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(0),
+                            bottomRight: Radius.circular(0),
+                            bottomLeft: Radius.circular(40),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 35),
+            Center(
+              child: Text(
+                'Get your Money',
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+            ),
+            Container(
+              child: Text(
+                'Under Control',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 20)),
+            Container(
+              child: Text(
+                'Manage your expenses',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Container(
+              child: Text('Seamlessly', style: TextStyle(color: Colors.white)),
+            ),
+            SizedBox(height: 300),
+
+            Container(
+              child: Center(
+                child: Text(
+                  'Sing Up with E-mail ID',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              width: 300,
+              height: 45,
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(9),
+                  topRight: Radius.circular(9),
+                  bottomLeft: Radius.circular(9),
+                  bottomRight: Radius.circular(9),
+                ),
+              ),
+            ),
+
+            Padding(padding: EdgeInsets.only(top: 15)),
+
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(right: 5, top: 2),
+                    child: Image.network(
+                      'https://img.icons8.com/?size=512&id=17949&format=png',
+                      width: 20,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Sing Up with Google',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+              width: 300,
+              height: 45,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(9),
+                  topRight: Radius.circular(9),
+                  bottomLeft: Radius.circular(9),
+                  bottomRight: Radius.circular(9),
+                ),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 30)),
+            Container(
+              child: Text(
+                'Already have an account? Sing in',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
